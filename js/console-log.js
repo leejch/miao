866e6363b7c1b648a52f3bfed77a9ce0fe6c432d
lijch <i@lijch.com>
2025-07-08 18:57:09 +0800

    值类型与运算符

---
+++ js/console-log.js
@@ -0,0 +1,13 @@
+console.log(2 * 5)
+console.log(Infinity) // Infinity
+console.log(typeof NaN) // number
+console.log(typeof true) // boolean
+console.log("Z" < "a") // true
+console.log(NaN == NaN) // false
+console.log(0 == -0) // true
+console.log(Infinity == Infinity) // true
+console.log(null == undefined) // true
+console.log(null == 0) // false
+console.log("" == false) // true
+console.log("" === false) // false
+console.log("" !== false) // true
